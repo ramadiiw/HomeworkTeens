6b14e5098e8b95ef66ca315a0986a8fda064ed34
ramadiiw <ramadianilham15@gmail.com>
2026-06-25 22:09:59 +0700

    scriptM13

---
+++ scriptM13.js
@@ -0,0 +1,14 @@
+//Meeting 13 Code Practice 2    
+
+const sum = (a, b) => {
+    return a + b;
+
+};
+
+console.log(sum(10, 20));
+console.log(sum(99, 1));
+
+//task 2
+const sum1 = (a, b) => a +b;
+console.log(sum1(10, 20));
+console.log(sum1(99, 1));
