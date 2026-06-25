f21808421f9a512bf306a9d9cdcf5fbc5a3e1242
ramadiiw <ramadianilham15@gmail.com>
2026-06-25 19:57:50 +0700

    scriptM4

---
+++ scriptM4.js
@@ -0,0 +1,20 @@
+//Task 1  Meeting 4
+console.log("Task 1 Meeting 4");
+let shoppingList = ["Wheat bread", "Pasta", "Tomato sauce", "Lowfat yogurt", "Butter"];
+let importanToBuy = shoppingList[0];
+
+shoppingList[3] = "Soy Milk";
+console.log("Update list: ",shoppingList);
+
+let listLength = shoppingList.length;
+console.log("how long is the array?: ",listLength);
+
+console.log();
+
+//Task 2 Meeting 4
+console.log("Task 2 Meeting 4");
+let holidayPlans = ["Paris", "Tokyo", "Sidney", "New York"];
+console.log("Initial holiday plans: ", holidayPlans);
+
+holidayPlans.push("Bali", "Hokkaido");
+console.log("Updated holiday plans: ", holidayPlans);
