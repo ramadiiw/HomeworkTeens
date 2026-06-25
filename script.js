794ef344a42d414fdffd1775328dc3704625fb17
ramadiiw <ramadianilham15@gmail.com>
2026-06-25 21:51:38 +0700

    updaet11

---
+++ script.js
@@ -0,0 +1,12 @@
+function rice() {
+    var menus = ["Risotto", "Fried Rice", "Sushi", "Bibimbap", "Curry Rice"];
+    var random = Math.floor(Math.random() * menus.length);
+    var recommendation = menus[random];
+    console.log(` I Recommend ${recommendation}`);
+}
+function noodle() {
+    var menus = ["Udon", "Pad Thai", "Mie Goreng", "Wonton Noodle Soup", "Laksa"];
+    var random = Math.floor(Math.random() * menus.length);
+    var recommendation = menus[random];
+    console.log(` I Recommend ${recommendation}`);
+}
